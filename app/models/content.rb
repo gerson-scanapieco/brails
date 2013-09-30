eae6f2bdd4e249f72375f9d822ed0cb83ef48fd1
Gerson Scanapieco <gerson@joshsoftware.com>
2013-09-30 20:54:43 +0530

    Fixes nitinzd/brails#67
    Uncommented Content Model line where the validation of topic_content field was defined.
    Added audio format parameter in Content factory line 11, where the topic_content audio file is defined. Without the format of the file, the object created by the factory would not pass all the field validations.

--- app/models/content.rb
+++ app/models/content.rb
@@ -18,7 +18,7 @@ class Content
   #validations
    validates :title, :content_body, :sq_no, :topic_id, :presence => true   
    validates :sq_no, :numericality => {:only_integer => true}
-   #validates_attachment :topic_content, :presence => true
+   validates_attachment :topic_content, :presence => true
    validates_attachment :topic_content, :content_type => {:content_type => ['audio/mpeg', 'audio/mp3']}
    validates_attachment :content_audio, :content_type => {:content_type => ['video/ogg', 'audio/ogg', 'application/ogg']}
 
